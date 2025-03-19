ecd388b47d9b3443cc67132fa362e4c80ac4df22
Exalted02 <exaltedsol02@gmail.com>
2025-03-19 13:57:37 +0530

    up

---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\CommonController;
+use App\Http\Controllers\LangController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmailManagementController;
+use App\Http\Controllers\EmailSettingsController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ChangePasswordController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+Route::get('clear-cache', function () {
+    \Artisan::call('config:cache');
+    \Artisan::call('cache:clear');
+	\Artisan::call('cache:clear');
+    // \Artisan::call('route:cache');
+    \Artisan::call('view:clear');
+    \Artisan::call('config:cache');
+    \Artisan::call('optimize:clear');
+	Log::info('Clear all cache');
+    dd("Cache is cleared");
+});
+Route::get('db-migrate', function () {
+    \Artisan::call('migrate');
+    dd("Database migrated");
+});
+Route::get('db-seed', function () {
+    \Artisan::call('db:seed');
+    dd("Database seeded");
+});
+Route::get('/', [ProfileController::class, 'welcome']);
+
+Route::get('lang/home', [LangController::class, 'index']);
+Route::get('lang/change', [LangController::class, 'change'])->name('changeLang');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+	
+
+
+Route::middleware('auth')->group(function () {
+	//ChangePassword
+	Route::get('/change-password', [ChangePasswordController::class, 'index'])->name('change-password');
+	Route::post('/change-password', [ChangePasswordController::class, 'save_data'])->name('change-password-save');
+
+	//EmailSettings
+	Route::get('/email-settings', [EmailSettingsController::class, 'index'])->name('user.email-settings');
+	Route::post('/email-settings', [EmailSettingsController::class, 'save_data'])->name('email-settings-save');
+
+// Email Management Routes
+	Route::get('email-management', [EmailManagementController::class,'index'])->name('email-management');
+	Route::get('/email-management-edit/{id}', [EmailManagementController::class, 'email_management_edit'])->name('email-management-edit');
+	Route::post('/email-management-edit-save',[EmailManagementController::class,'manage_email_management_process'])->name('email-management-edit-save');
+});
+
+
+
+require __DIR__.'/auth.php';
